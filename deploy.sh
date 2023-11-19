5b80e7abf4add99778a97c4e19093d488fe3ddb9
Ahyoung <ahyoung227@gmail.com>
2023-11-19 01:51:02 -0500

    changed text

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+[ -d /home/ubuntu/action ] || mkdir -p /home/ubuntu/action
+echo "> current time: $(date)" >> /home/ubuntu/action/deploy.log
+
+echo "> check running application" >> /home/ubuntu/action/deploy.log
+
+# pm2 list 명령어를 사용하여 실행 중인 프로세스 리스트 가져오기
+PM2_LIST=$(pm2 list) >> /home/ubuntu/action/deploy.log
+
+# pm2로 애플리케이션을 시작합니다.
+echo "> pm2 start node-test" >> /home/ubuntu/action/deploy.log
+pm2 kill
+sleep 5
+pm2 start ./bin/www
+
+PM2_LIST=$(pm2 list)
+
+echo "현재 구동 중인 애플리케이션 리스트:"
+echo "$PM2_LIST" >> /home/ubuntu/action/deploy.log
